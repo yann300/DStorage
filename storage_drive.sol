bdcb074d5a432ded5c16e438bf3f3c9830ce45df
yann300 <yann.levreau@gmail.com>
2016-02-07 12:45:47 +0100

    initial commit

---
+++ storage_drive.sol
@@ -0,0 +1,66 @@
+//Sample contract
+contract storage_drive
+{
+	itmap st;
+	address owner;
+	
+	function storage_drive() 
+	{
+		owner = msg.sender;
+	}
+	
+	function set(bytes _k, bytes _v) 
+	{
+		if (msg.sender != owner)
+			return;
+		insert(st, _k, _v);
+	}
+	
+	function getValue(bytes _k) returns (bytes)
+	{
+		return st.data[_k].value;
+	}
+		
+	function remove(bytes _k)
+	{
+		if (msg.sender != owner)
+			return;
+		remove(st, _k);
+	}
+	
+	struct itmap
+ 	{
+    	mapping(bytes => IndexValue) data;
+    	KeyFlag[] keys;
+    	uint size;
+  	}
+  
+	struct IndexValue { uint keyIndex; bytes value; }
+	struct KeyFlag { bytes key; bool deleted; }
+	function insert(itmap storage self, bytes key, bytes value) internal returns (bool replaced)
+	{
+		uint keyIndex = self.data[key].keyIndex;
+		self.data[key].value = value;
+		if (keyIndex > 0)
+		return true;
+		else
+		{
+			keyIndex = self.keys.length++;
+			self.data[key].keyIndex = keyIndex + 1;
+			self.keys[keyIndex].key = key;
+			self.size++;
+			return false;
+		}
+	}
+	
+	function remove(itmap storage self, bytes key) internal returns (bool success)
+	{
+		uint keyIndex = self.data[key].keyIndex;
+		if (keyIndex == 0)
+		return false;
+		delete self.data[key];
+		self.keys[keyIndex - 1].deleted = true;
+		self.size --;
+	}
+}	
+
